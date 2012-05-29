ca6ad815ac7bb8130ea6ea740af05f8fbfc8ce4f
unknown <Tiago Vicente@Tecra-TiagoSNSI.IMOMOOVE.LOCAL>
2012-05-29 16:21:11 +0100

    versao orginial

--- DCOproj/src/ui/MainFrame.java
+++ DCOproj/src/ui/MainFrame.java
@@ -394,7 +394,11 @@ public class MainFrame extends JFrame implements ActionListener, Observer {
     protected void addTag(String tag) {
         Main.lib.addTagToPicture(tag);
     }
-
+    
+    protected void removeTag(String tag) {
+        Main.lib.removeTagFromPicture(tag);
+    }
+    
     protected void recoverLastTagSelection() {
         Main.lib.recoverLastTagSelection();
     }
